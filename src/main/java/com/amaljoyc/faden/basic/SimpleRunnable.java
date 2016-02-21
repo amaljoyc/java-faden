bf7cc0776f0f97d8d5717b792ac039a729b4ebc4
amaljoyc <amaljoyc@gmail.com>
2016-02-21 17:49:14 +0100

    basic threading

--- src/main/java/com/amaljoyc/faden/basic/SimpleRunnable.java
+++ src/main/java/com/amaljoyc/faden/basic/SimpleRunnable.java
@@ -1,4 +1,29 @@
 package com.amaljoyc.faden.basic;
 
 public class SimpleRunnable {
+
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName());
+
+        Thread t1 = new Thread(new Runner2());
+        t1.start();
+
+        Thread t2 = new Thread(new Runner2());
+        t2.start();
+    }
+}
+
+
+class Runner2 implements Runnable {
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName() + ", " + "Value=" + i);
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- src/main/java/com/amaljoyc/faden/basic/SimpleThread.java
+++ src/main/java/com/amaljoyc/faden/basic/SimpleThread.java
@@ -1,4 +1,28 @@
 package com.amaljoyc.faden.basic;
 
 public class SimpleThread {
+
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName());
+
+        Thread t1 = new Runner1();
+        t1.start();
+
+        Thread t2 = new Runner1();
+        t2.start();
+    }
+}
+
+class Runner1 extends Thread {
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(this.getName() + ", " + "Value=" + i);
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
